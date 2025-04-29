eb831b3beb22f545382adb3ebcde083085c82945
Gabriel Simini <127317161+GalSimini@users.noreply.github.com>
2025-04-29 17:50:39 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Banner from './components/Banner/Banner';
+import CampoTexto from './components/CampoTexto/CampoTexto';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <CampoTexto label="Nome"/>  
+      <CampoTexto label="Cargo"/>  
+      <CampoTexto label="Imagem"/>  
+
+    </div>
+  );
+}
+
+export default App;
