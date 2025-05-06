67ae2f66a11150b2eaa6d3be1e9385f859218451
GalSimini <gbielsimini@gmail.com>
2025-05-06 10:44:31 -0300

    Add required attribute to CampoTexto inputs and implement form submission handler

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,13 +15,18 @@ function Form(props) {
         'Inovação e Gestão'
     ]
 
+    const onSave = (evento) => {
+        evento.preventDefault()
+        console.log('Formulário enviado')
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={onSave}>
                 <h2>{props.label}</h2>
-                <CampoTexto label="Nome" placeholder="digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="informe o endereço da imagem" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="digite seu cargo" />
+                <CampoTexto  label="Imagem" placeholder="informe o endereço da imagem" />
                 <ListaSuspensa itens={times} label="Times"/>
                 <Button>
                     Criar Card
