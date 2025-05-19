854cfd05689ba3311cb19d29b4fffb23a9f37434
Gabriel Simini <127317161+GalSimini@users.noreply.github.com>
2025-05-19 14:02:12 -0300

    Add teams feature with dynamic rendering in App component
    - Introduced a times array in App.js to define different teams with their respective colors.
    - Updated the App component to render Team components dynamically based on the times array.
    - Modified the Team component to accept and display team name and colors, enhancing the visual representation of each team.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Team from './components/Team/Team';
 
 function App() {
 
+  const times = [
+    { nome: 'Programação', corPrimaria: '#57C278', corSecundaria: '#D9F7E9' },
+    { nome: 'Frontend', corPrimaria: '#82CFFA', corSecundaria: '#E8F8FF' },
+    { nome: 'Data Science', corPrimaria: '#A6D157', corSecundaria: '#F0F8E2' },
+    { nome: 'Devops', corPrimaria: '#E06B69', corSecundaria: '#FDE7E8' },
+    { nome: 'UX e Design', corPrimaria: '#DB6EBF', corSecundaria: '#FAE9F5' },
+    { nome: 'Mobile', corPrimaria: '#FFBA05', corSecundaria: '#FFF5D9' },
+    { nome: 'Inovação e Gestão', corPrimaria: '#FF8A29', corSecundaria: '#FFEEDF' }
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
   const aoNovoColaboradorAdd = (colaborador) => {
     setColaboradores([...colaboradores, colaborador])
@@ -14,7 +24,14 @@ function App() {
     <div className="App">
       <Banner/>
       <Form aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdd(colaborador)}/>
-      <Team title='teste'/>
+      
+      {times.map(time => <Team 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+      />)}
+
     </div>
 );}
 
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,11 +1,11 @@
 import './Team.css';
 
-function Team(props) {  
+function Team(props) {
 
     return (
-        <section className='team'>
-            <h3 className='team-title'>{props.title}</h3>
-            <div className='team-members'>
+        <section className='team' style={{ backgroundColor: props.corSecundaria }}>
+            <h3 className='team-title' style={{ borderBottom: `4px solid ${props.corPrimaria}` }}>{props.nome}</h3>
+            <div>
 
             </div>
         </section>
